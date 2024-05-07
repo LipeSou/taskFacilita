131db96f6d6ed0984d3d120626a8d83111a6fab3
LipeSou <felipesoutomdt@gmail.com>
2024-05-07 17:12:16 -0300

    feat: config stylus and global variables

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      stylus: {
+        additionalData: `@import './global/variables.styl'`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
